63e655adc66dd0b80a6a7bee4f7456c8b712bbbc
Floarea George-Marian <39678100+GeorgeF23@users.noreply.github.com>
2020-12-04 15:53:46 +0200

    Solved the problem in C

--- IOCLA/Tema3/test.c
+++ IOCLA/Tema3/test.c
@@ -4,13 +4,69 @@
 #include <string.h>
 #define COUNT 10 
   
-// A binary tree node 
+char delim[] = " \n";
+
 struct Node 
 { 
     char *data; 
     struct Node* left, *right; 
 }; 
   
+// A structure to represent a stack
+struct Stack {
+    int top;
+    unsigned capacity;
+    struct Node** array;
+};
+ 
+// function to create a stack of given capacity. It initializes size of
+// stack as 0
+struct Stack* createStack(unsigned capacity)
+{
+    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->array = (struct Node**)malloc(stack->capacity * sizeof(struct Node*));
+    return stack;
+}
+ 
+// Stack is full when top is equal to the last index
+int isFull(struct Stack* stack)
+{
+    return stack->top == stack->capacity - 1;
+}
+ 
+// Stack is empty when top is equal to -1
+int isEmpty(struct Stack* stack)
+{
+    return stack->top == -1;
+}
+ 
+// Function to add an item to stack.  It increases top by 1
+void push(struct Stack* stack, struct Node* item)
+{
+    if (isFull(stack))
+        return;
+    stack->array[++stack->top] = item;
+}
+ 
+// Function to remove an item from stack.  It decreases top by 1
+struct Node *pop(struct Stack* stack)
+{
+    if (isEmpty(stack))
+        return 0;
+    return stack->array[stack->top--];
+}
+ 
+// Function to return the top from stack without removing it
+struct Node *peek(struct Stack* stack)
+{
+    if (isEmpty(stack))
+        return 0;
+    return stack->array[stack->top];
+}
+
+// A binary tree node 
 // Helper function to allocates a new node 
 struct Node* newNode(char *data) 
 { 
@@ -70,25 +126,32 @@ int isOperator(char *token){
 }
 
 struct Node* construct_tree(char *token, int *index){
-    if(token[*index] == 0){
-        return NULL;
-    }
-    char *data = token + *index;
-    while(token[*index] != ' ' && token[*index] != 0){
-        (*index)++;
-    }
-    token[*index] = 0;
-    (*index)++;
-
-    struct Node *node = newNode(data);
+    struct Stack *stack = createStack(100);
     
-    if(isOperator(data)) {
-        node->left = construct_tree(token, index);
-        node->right = construct_tree(token, index);
+    char *p = strtok(token, delim);
+
+    struct Node *root = newNode(p);
+    push(stack, root);
+    p = strtok(NULL, delim);
+
+    while(p){
+        struct Node *last = pop(stack);
+        struct Node *new_node = newNode(p);
+
+        if(last->left == NULL){
+            last->left = new_node;
+            push(stack, last);
+        } else {
+            last->right = new_node;
+        }
+
+        if(isOperator(p)){
+            push(stack, new_node);
+        }
+        
+        p = strtok(NULL, delim);
     }
-
-
-    return node;
+    return root;
 }
 
 // Driver program to test above functions 
