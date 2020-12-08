ac0e7f51b37ea25f157b02424a40ada4745fb26a
Floarea George-Marian <39678100+GeorgeF23@users.noreply.github.com>
2020-12-08 13:32:21 +0200

    Fix iocla_atoi
    The function was not handling negative numbers

--- IOCLA/Tema3/ast.asm
+++ IOCLA/Tema3/ast.asm
@@ -24,22 +24,36 @@ iocla_atoi:
     push ebp
     mov ebp, esp
 
+    push ebx
     mov ecx, [ebp + 8]  ; token
     xor eax, eax        ; numarul convertit
+    xor ebx, ebx        ; retine 1 daca e negativ, 0 daca e pozitiv
+
+    movzx edx, byte [ecx]
+    cmp edx, '-'
+    jne start
+    inc ecx
+    mov ebx, 1      ; marchez numarul ca fiind negativ
 
 start:
     movzx edx, byte [ecx]
     inc ecx
     cmp edx, '0'
-    jb end_atoi
+    jb check_negative
     cmp edx, '9'
-    jg end_atoi
+    jg check_negative
     sub edx, '0'
     imul eax, 10
     add eax, edx
     jmp start
 
+check_negative:
+    cmp ebx, 1
+    jne end_atoi
+    neg eax
+
 end_atoi:
+    pop ebx
     leave
     ret
 
@@ -93,16 +107,23 @@ create_tree:
 start_while:
     push delim
     push 0x00000000
-    call strtok     ; extrag urmatorul operator
+    call strtok     ; extrag urmatorul element din token
     add esp, 8      ; refac stiva
 
     cmp eax, 0
     je end_create_tree
     pop ebx         ; scot ultimul nod pus pe stiva
+    movzx ecx, byte [eax]   ; salvez primul caracter din element
+    movzx edx, byte [eax + 1]   ; salvez al doilea caracter din element
 
+    push ecx
+    push edx
     push eax
     call create_new_node    ; creez nodul nou
     add esp, 4      ; refac stiva
+
+    pop edx
+    pop ecx
 
     cmp dword [ebx + 4], 0
     jne put_right
@@ -114,13 +135,20 @@ put_right:
     mov [ebx + 8], eax
 
 check_operator:
-    nop
+    cmp ecx, '/'
+    jg start_while
+    cmp edx, 0
+    jne start_while
+
+    push eax
 
     jmp start_while
 
 end_create_tree:
     pop ebx     ; restaurez ebx
     pop esi     ; restaurez esi
+
+
     mov eax, dword [root]   ; salvez valoarea de return
     leave
     ret
