afcb5355e9b9c53c436936f22df1ce9418a5838b
Floarea George-Marian <39678100+GeorgeF23@users.noreply.github.com>
2020-12-08 11:59:09 +0200

    Created create_new_node function
    The function allocs space in memory for a new node and copies the data field to the new node.

--- IOCLA/Tema3/ast.asm
+++ IOCLA/Tema3/ast.asm
@@ -1,10 +1,9 @@
 section .data
     delim db " ", 0xA, 0
-    my_str db "123", 0
     format db "%s", 0xA, 0
 
-section .bss
-    root resd 1
+    node_size dd 12
+    root dd 0
 
 section .text
 
@@ -13,9 +12,13 @@ extern print_tree_inorder
 extern print_tree_preorder
 extern evaluate_tree
 extern printf
+extern strtok
+extern malloc
+extern strdup
 
 global create_tree
 global iocla_atoi
+global create_new_node
 
 iocla_atoi: 
     push ebp
@@ -40,16 +43,51 @@ end:
     leave
     ret
 
+create_new_node:
+    push ebp
+    mov ebp, esp
+
+    push ebx
+    mov ebx, [ebp + 8]  ; data
+
+    push dword [node_size]
+    call malloc     ; aloc memorie pentru noul nod
+    add esp, 4
+
+    push eax        ; salvez adresa nodului
+    push ebx        ; pun data pe stiva
+    call strdup     ; duplic sirul data
+    add esp, 4      ; refac stiva
+    mov ebx, eax    ; salvez rezultatul lui strdup
+    pop eax         ; reiau de pe stiva adresa nodului
+
+    mov dword [eax], ebx    ; setez data
+    mov dword [eax + 4], 0  ; setez left
+    mov dword [eax + 8], 0  ; setez right
+
+    pop ebx
+    leave
+    ret
 
 create_tree:
     push ebp
     mov ebp, esp
+    
+    push esi    ; salvez esi
+    mov esi, [ebp + 8]  ; token
 
-    push my_str
-    call check_atoi
+    push delim
+    push esi
+    call strtok     ; scot primul operator
+    add esp, 8      ; refac stiva
 
-    mov esi, [ebp + 8]
+    push eax    ; parametrul functiei create_new_node
+    call create_new_node    ; creez nodul nou
+    add esp, 4  ; refac stiva
 
-    xor eax, eax
+
+    mov dword [root], eax
+    pop esi     ; restaurez esi
+    mov eax, dword [root]   ; salvez valoarea de return
     leave
     ret
