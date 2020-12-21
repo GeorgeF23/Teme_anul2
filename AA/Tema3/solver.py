2dc337a4e85a4ae62b0b23ec5574ea8eb7f7fe54
Floarea George-Marian <39678100+GeorgeF23@users.noreply.github.com>
2020-12-21 21:32:45 +0200

    Solved problem v1.0.0

--- AA/Tema3/solver.py
+++ AA/Tema3/solver.py
@@ -1,9 +1,9 @@
 def extract_variables_from_formula(formula):
     return list(set([variable.replace('~', '') for sublist in [literal.split("V") for literal in [clause[1:-1] for clause in formula.split("^")]] for variable in sublist]))
 
-def convert_to_matrix(formula):
+
+def convert_to_matrix(variables, formula):
     matrix = []
-    variables = extract_variables_from_formula(formula)
 
     clauses = formula.split("^")
     for clause in clauses:
@@ -18,13 +18,31 @@ def convert_to_matrix(formula):
     return matrix
 
 
-def solver(formula):
+def solver(formula, found_callback, not_found_callback):
     variables = extract_variables_from_formula(formula)
+    matrix = convert_to_matrix(variables, formula)
+
+    found = False
+    def check_clause(clause, interpretation):
+        for literal_index, literal_value in enumerate(clause):
+            if literal_value == 1 and variables[literal_index] in interpretation:
+                return True
+            elif literal_value == -1 and variables[literal_index] not in interpretation:
+                return True
+        return False
+
+    def check_interpretation(interpretation):
+        for clause in matrix:
+            if not check_clause(clause, interpretation):
+                return False
+        return True
 
     def backtracking(s, index):
-        
-        print(s)
-
+        nonlocal found
+        if found:
+            return
+        if check_interpretation(s):
+            found = True
         for i in range(index, len(variables)):
             s.append(variables[i])
             backtracking(s, i + 1)
@@ -32,8 +50,16 @@ def solver(formula):
 
     backtracking([], 0)
 
+    found_callback() if found else not_found_callback()
+
+
+
+def found_interpretation():
+    print("Found interpretation")
+
+def not_found_interpretation():
+    print("Not found")
 
 with open("input.txt") as f:
     formula = f.read()
-    matrix = convert_to_matrix(formula)
-    solver(formula)
+    solver(formula, found_interpretation, not_found_interpretation)
