da12927a9c1dea5eb4b3537745fda899718be87b
Floarea George-Marian <39678100+GeorgeF23@users.noreply.github.com>
2020-12-22 22:41:57 +0200

    Moved functions to "formula_parser.py".
    Moved extract_variables_from_formula, convert_to_matrix to a separate file so they can be used later.

--- AA/Tema3/sat.py
+++ AA/Tema3/sat.py
@@ -1,35 +1,4 @@
-import re
-
-
-def extract_variables_from_formula(formula):
-    return list(
-        set(
-            [variable.replace('~', '')
-             for sublist
-             in [literal.split("V")
-                 for literal in
-                 [re.sub("[()]", "", clause)
-                  for clause in
-                  formula.split("^")]] for
-             variable in sublist]
-        )
-    )
-
-
-def convert_to_matrix(variables, formula):
-    matrix = []
-
-    clauses = formula.split("^")
-    for clause in clauses:
-        line = [0] * len(variables)
-        for literal in re.sub("[()]", "", clause).split("V"):
-            if(literal.startswith("~")):
-                line[variables.index(literal[1:])] = -1
-            else:
-                line[variables.index(literal)] = 1
-        matrix.append(line)
-
-    return matrix
+from formula_parser import extract_variables_from_formula, convert_to_matrix
 
 
 def solver(formula, found_callback, not_found_callback):
