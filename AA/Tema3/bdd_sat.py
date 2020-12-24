79347e17032c0244e8abdd3008875dff870a682f
Floarea George-Marian <39678100+GeorgeF23@users.noreply.github.com>
2020-12-24 17:10:37 +0200

    Added comments

--- AA/Tema3/bdd_sat.py
+++ AA/Tema3/bdd_sat.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 
 
 def remove_duplicate_rows(matrix):
+    """Removes duplicate rows in a matrix"""
     if type(matrix) is not list:
         return matrix
     new_matrix = []
@@ -12,7 +13,8 @@ def remove_duplicate_rows(matrix):
     return new_matrix
 
 
-def matrix_contains_lists(matrix):
+def is_matrix(matrix):
+    """Checks if argument is matrix"""
     for row in matrix:
         if type(row) is list:
             return True
@@ -26,48 +28,53 @@ class Node:
         self.right = None
 
     def evaluate_left(self):
+        """Evaluates the current formula when the first variable is False and places the new formula in the left child"""
         formula_copy = deepcopy(self.current_formula)
         left_formula = []
         for clause in formula_copy:
-            if type(clause) is not list:
-                continue
-
-            if len(clause) == 1:
+            if len(clause) == 1:    # If the clause contains only one literal, replace the whole clause with it's value (True / False)
                 if clause[0] == -1:
                     left_formula.append(True)
                 elif clause[0] == 1:
                     left_formula.append(False)
                 elif clause[0] == 0:
                     left_formula.append(False)
+            # If the first literal in clause is not negated, append the rest of the clause
             elif clause[0] == 1 or clause[0] == 0:
                 left_formula.append(clause[1:])
 
-        if left_formula == []:
+        if left_formula == []:  # If the formula remains empty, it means it is True
             left_formula.append(True)
+        # Create the left child with the remaining formula
         self.left = Node(remove_duplicate_rows(left_formula))
 
     def evaluate_right(self):
+        """Evaluates the current formula when the first variable is True and places the new formula in the right child"""
         formula_copy = deepcopy(self.current_formula)
         right_formula = []
         for clause in formula_copy:
-            if type(clause) is not list:
-                continue
-            if len(clause) == 1:
+
+            if len(clause) == 1:    # If the clause contains only one literal, replace the whole clause with it's value (True / False)
                 if clause[0] == -1:
                     right_formula.append(False)
                 elif clause[0] == 1:
                     right_formula.append(True)
                 elif clause[0] == 0:
                     right_formula.append(False)
+            # If the first literal in clause is negated, append the rest of the clause
             elif clause[0] == -1 or clause[0] == 0:
                 right_formula.append(clause[1:])
 
-        if right_formula == []:
+        if right_formula == []:  # If the formula remains empty, it means it is True
             right_formula.append(True)
+        # Create the right child with the remaining formula
         self.right = Node(remove_duplicate_rows(right_formula))
 
 
 def solver(formula, found_callback, not_found_callback):
+    """Checks if a formula is satisfiable and calls the apropriate callback"""
+
+    # Convert the formula into it's matrix form
     variables = extract_variables_from_formula(formula)
     matrix = convert_to_matrix(variables, formula)
 
@@ -76,17 +83,19 @@ def solver(formula, found_callback, not_found_callback):
 
     def dfs(node, depth):
         nonlocal is_satisfiable
-        if not matrix_contains_lists(node.current_formula):
-            if False not in node.current_formula:
+        # If the formula is not a matrix anymore, it means it is done with the recursion
+        if not is_matrix(node.current_formula):
+            if False not in node.current_formula:   # If there is no 'False' in the formula, it's satisfiable
                 is_satisfiable = True
 
         if is_satisfiable == True:
             return
 
         if depth < len(variables):
-            node.evaluate_left()
-            node.evaluate_right()
+            node.evaluate_left()    # Calculate the left child
+            node.evaluate_right()   # Calculate the right child
 
+            # Recursive calls to the next level
             dfs(node.left, depth + 1)
             dfs(node.right, depth + 1)
 
