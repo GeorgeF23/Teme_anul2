ae4f63066daecc809aa97ffd8de82db13043b1de
Floarea George-Marian <39678100+GeorgeF23@users.noreply.github.com>
2020-12-24 19:40:48 +0200

    Implemented ProductBuilder pattern

--- POO/Tema2/src/com/george/Product.java
+++ POO/Tema2/src/com/george/Product.java
@@ -7,4 +7,28 @@ public class Product {
     private double price;
     private int quantity;
     private Discount discount;
+
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDiscount(Discount discount) {
+        this.discount = discount;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
