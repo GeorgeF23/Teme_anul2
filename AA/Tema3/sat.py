7728b7415d599fad052e30a4dbf1256ca2fd6d9d
Floarea George-Marian <39678100+GeorgeF23@users.noreply.github.com>
2020-12-24 17:14:53 +0200

    Add comments and remove main block

--- AA/Tema3/sat.py
+++ AA/Tema3/sat.py
@@ -2,12 +2,16 @@ from formula_parser import extract_variables_from_formula, convert_to_matrix
 
 
 def solver(formula, found_callback, not_found_callback):
+    """Checks if a formula is satisfiable and calls the apropriate callback"""
+
+    # Convert the formula into it's matrix form
     variables = extract_variables_from_formula(formula)
     matrix = convert_to_matrix(variables, formula)
 
     found = False
 
     def check_clause(clause, interpretation):
+        """ Checks if an interpretation satisfies a clause """
         for literal_index, literal_value in enumerate(clause):
             if literal_value == 1 and variables[literal_index] in interpretation:
                 return True
@@ -16,6 +20,7 @@ def solver(formula, found_callback, not_found_callback):
         return False
 
     def check_interpretation(interpretation):
+        """ Checks if an interpretation satisfies the formula"""
         for clause in matrix:
             if not check_clause(clause, interpretation):
                 return False
@@ -25,8 +30,10 @@ def solver(formula, found_callback, not_found_callback):
         nonlocal found
         if found:
             return
-        if check_interpretation(s):
+        if check_interpretation(s):  # If found a good interpretation, stop
             found = True
+
+        # Generate all subsets from variables
         for i in range(index, len(variables)):
             s.append(variables[i])
             backtracking(s, i + 1)
@@ -35,17 +42,3 @@ def solver(formula, found_callback, not_found_callback):
     backtracking([], 0)
 
     found_callback() if found else not_found_callback()
-
-
-def found_interpretation():
-    print("Found interpretation")
-
-
-def not_found_interpretation():
-    print("Not found")
-
-
-if __name__ == '__main__':
-    with open("input.txt") as f:
-        formula = f.read()
-        solver(formula, found_interpretation, not_found_interpretation)
