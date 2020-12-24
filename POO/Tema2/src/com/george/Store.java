9b586c3eec0554fdd996546d142547ab35e1ac3b
Floarea George-Marian <39678100+GeorgeF23@users.noreply.github.com>
2020-12-24 19:26:25 +0200

    Implemented Singleton pattern to Store

--- POO/Tema2/src/com/george/Store.java
+++ POO/Tema2/src/com/george/Store.java
@@ -1,8 +1,18 @@
 package com.george;
 
 public class Store {
+    private static Store instance;
+
     private String name;
     private Currency currency;
     private Product[] products;
     private Manufacturer[] manufacturers;
+
+
+    public static Store getInstance() {
+        if (instance == null) {
+            instance = new Store();
+        }
+        return instance;
+    }
 }
