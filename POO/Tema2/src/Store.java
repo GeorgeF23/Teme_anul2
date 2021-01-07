1c94e9cf4ca54a95b2c9bf523392995be4be30f9
Floarea George-Marian <39678100+GeorgeF23@users.noreply.github.com>
2021-01-07 13:23:08 +0200

    Created Store class and implemented Singleton

--- POO/Tema2/src/Store.java
+++ POO/Tema2/src/Store.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Store {
+    private Store instance;
+
+    private ArrayList<Currency> currencies;
+    private Currency currentCurrency;
+
+    public Store getInstance(){
+        if(instance == null){
+            instance = new Store();
+        }
+        return instance;
+    }
+
 }
