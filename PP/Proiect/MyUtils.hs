3fda64cca70afd6425efef9c53751bab4dbd75a0
George Floarea <floareageorge@gmail.com>
2021-04-02 20:26:51 +0300

    Solved task 4

--- PP/Proiect/MyUtils.hs
+++ PP/Proiect/MyUtils.hs
@@ -1,4 +1,11 @@
-module MyUtils(string_to_float) where
+module MyUtils(string_to_float, string_to_integer, increment_field) where
 
 string_to_float :: String -> Float
 string_to_float str = if null str then 0.0 else read str :: Float
+
+string_to_integer :: String -> Integer
+string_to_integer str = if null str then 0 else read str :: Integer
+
+
+increment_field :: Int -> [Integer] -> [Integer]
+increment_field field_no list = take field_no list ++ [list !! max 0 field_no + 1] ++ drop (min (length list) field_no + 1) list
--- PP/Proiect/Tasks.hs
+++ PP/Proiect/Tasks.hs
@@ -21,7 +21,7 @@ type Table = [Row]
 
 -- Task 1
 compute_exam_grades :: Table -> Table
-compute_exam_grades = (["Nume","Punctaj Exam"] :) . map (\line -> head line : [show $ compute_one_exam_grade $ tail line]) . tail 
+compute_exam_grades = (["Nume","Punctaj Exam"] :) . map (\line -> head line : [show $ compute_one_exam_grade $ tail line]) . tail
 
 
 -- Primeste lista de note si returneaza nota finala din examen
@@ -31,27 +31,41 @@ compute_one_exam_grade = foldr (\el acc -> if acc == (-1) then string_to_float e
 -- Task 2
 -- Number of students who have passed the exam:
 get_passed_students_num :: Table -> Int
-get_passed_students_num = undefined
+get_passed_students_num = length . filter studentPassed . tail . compute_exam_grades
+    where
+        studentPassed :: Row -> Bool
+        studentPassed = (>= 2.5) . string_to_float . head . tail
+
 
 -- Percentage of students who have passed the exam:
 get_passed_students_percentage :: Table -> Float
-get_passed_students_percentage = undefined
+get_passed_students_percentage grades = fromIntegral (get_passed_students_num grades) / fromIntegral (length (tail grades))
 
 -- Average exam grade
 get_exam_avg :: Table -> Float
-get_exam_avg = undefined
+get_exam_avg grades = sum (map (compute_one_exam_grade . tail) $ tail grades) / fromIntegral (length $ tail grades)
 
 -- Number of students who gained at least 1.5p from homework:
 get_passed_hw_num :: Table -> Int
-get_passed_hw_num = undefined
+get_passed_hw_num = length . filter (>= 1.5) . map (sum . map string_to_float . take 3 . tail . tail) . tail
 
 -- Task 3
 get_avg_responses_per_qs :: Table -> Table
-get_avg_responses_per_qs = undefined
+get_avg_responses_per_qs grades = ["Q1","Q2","Q3","Q4","Q5","Q6"] : [map (show . (/ (fromIntegral $ length $ tail grades))) (foldl (zipWith (+)) [0.0, 0.0, 0.0, 0.0, 0.0, 0.0] $ map( map string_to_float . take 6 . tail) $ tail grades)]
 
 -- Task 4
-get_exam_summary :: Table -> Table
-get_exam_summary = undefined
+-- get_exam_summary :: Table -> Table
+get_exam_summary = construct_table . foldl (zipWith increment_question_numbers) [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]] . map( map string_to_integer . take 6 . tail) . tail
+    where
+        increment_question_numbers :: [Integer] -> Integer -> [Integer]
+        increment_question_numbers acc question_points
+            | question_points == 0 = increment_field 0 acc
+            | question_points == 1 = increment_field 1 acc
+            | otherwise = increment_field 2 acc
+
+        construct_table :: [[Integer]] -> Table
+        construct_table = (["Q","0","1","2"] :) . zipWith (:) ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6"] . map (map show)
+
 
 -- Task 5
 get_ranking :: Table -> Table
