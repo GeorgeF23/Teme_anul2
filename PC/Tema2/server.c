fcf0d39eeaacdf1087e587f2680068ee57e88d5b
George Floarea <floareageorge@gmail.com>
2021-05-01 12:54:35 +0300

    Added IO multiplexing

--- PC/Tema2/server.c
+++ PC/Tema2/server.c
@@ -23,14 +23,52 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
+    // Initialize descriptor sets
+    int fdmax;
+    fd_set read_fds;
+    fd_set tmp_fds;
+    FD_ZERO(&read_fds);
+    FD_ZERO(&tmp_fds);
+
+    FD_SET(STDIN_FILENO, &read_fds); // Add stdin into set
+
     int udp_fd = init_udp_listener(atoi(argv[1]));
     DIE(udp_fd == -1, "init_udp_listener");
     
-    while(1) {
-        struct message *msg = recieve_message(udp_fd);
-        DIE(msg == NULL, "receive_message");
+    FD_SET(udp_fd, &read_fds);  // Add udp_fd into set
+    fdmax = udp_fd; // Update the biggest file descriptor
 
-        printf ("Primit mesaj: %s\n", msg->topic);
+    while(1) {
+        tmp_fds = read_fds; // Create copy of fd set
+
+        // Get the fd's with incoming data
+        int ret = select(fdmax + 1, &tmp_fds, NULL, NULL, NULL);
+        DIE(ret < 0, "select");
+
+        for (int i = 0; i <= fdmax; i++) {
+            if (FD_ISSET(i, &tmp_fds)) {
+                if (i == STDIN_FILENO) {
+                    // Incoming data from stdin
+                    char buffer[16];
+                    scanf("%s", buffer);
+
+                    if (strcmp(buffer, "exit") == 0) {
+                        goto end;
+                    } else {
+                        fprintf(stderr, "Invalid input!\n");
+                    }
+                } else if (i == udp_fd) {
+                    // Incoming data from udp socket
+                    struct message *msg = recieve_message(udp_fd);
+                    DIE(msg == NULL, "receive_message");
+
+                    printf ("Primit mesaj: %s\n", msg->topic);
+                }
+            }
+        }
     }
+
+end:
+    close(udp_fd);
     return 0;
 }
