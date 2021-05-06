d9c60050208235944eeb2aa2050902feea9646e9
George Floarea <floareageorge@gmail.com>
2021-05-06 11:05:41 +0300

    Moved header files in separate directory

--- PC/Tema2/include/tcp.h
+++ PC/Tema2/include/tcp.h
@@ -11,6 +11,15 @@
 #include "message.h"
 
 #define MAX_CLIENTS 1000
+#define MAX_CLIENT_ID_LEN 11
+
+struct client_info {
+    int socket;
+    char id[MAX_CLIENT_ID_LEN];
+
+    struct sockaddr_in socket_info;
+    socklen_t socket_info_len;
+};
 
 /**
  * @brief  Creates a tcp socket that listens for new connections
